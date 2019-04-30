4dcf677874a213bf12f52c443338db69f139b6b9
Tyler Miller <tyler@tdmilleranalytics.com>
2019-04-30 14:50:05 -0700

    add robots.txt and config nginx

--- tdmilleranalytics/urls.py
+++ tdmilleranalytics/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', contact_form.urls),
     path('', include('tdmilleranalytics_resume.urls'))
 ]
