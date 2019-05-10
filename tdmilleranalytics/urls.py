8d23c35ecbefd1cb5b14413d280f48f78e4e2bc3
Tyler Miller <tyler@tdmilleranalytics.com>
2019-05-10 13:02:14 -0700

    remove contact from urls

--- tdmilleranalytics/urls.py
+++ tdmilleranalytics/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', contact_form.urls),
     path('', include('tdmilleranalytics_resume.urls'))
 ]
